ffe392f4d09870c23cfaf49f7392cee8d60f38b9
ToanKitsu <nguyenngoctoanrc@gmail.com>
2023-06-13 18:46:39 +0700

    init commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit similique
+        sed ullam rem eveniet at necessitatibus, recusandae voluptas, blanditiis
+        exercitationem fugiat commodi temporibus iusto totam, sunt quisquam ad
+        reiciendis ut perspiciatis corrupti nam voluptate quidem mollitia!
+        Corrupti temporibus nesciunt assumenda.
+      </p>
+    </section>
+  );
+};
+
+export default About;
